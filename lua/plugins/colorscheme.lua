2e69fb14cf2b12207a716962f06b589a9adec5cd
Binciu Viorel <viorel.binciu@pdq.com>
2024-05-25 12:36:10 +0300

    feat: Added colorscheme

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,
+      style = "night",
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+        comments = { italic = true },
+        keywords = { italic = true },
+      },
+      on_highlights = function(hl, colors)
+        hl.LineNr = { bg = "none", fg = "#999999" }
+        hl.CursorLineNr = { fg = "#ffffff" }
+      end,
+    },
+  },
+  {
+    "Mofiqul/vscode.nvim",
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
