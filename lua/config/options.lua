109669c09b22bad85638e3d66d8e61892b54bffc
Binciu Viorel <viorel.binciu@pdq.com>
2024-05-25 12:30:02 +0300

    feat: Added clipboard copy

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,17 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Clipboard copy --
+vim.g.clipboard = {
+  name = "wslclipboard",
+  copy = {
+    ["+"] = "wsl.exe clip.exe",
+    ["*"] = "wsl.exe clip.exe",
+  },
+  paste = {
+    ["+"] = "wsl.exe powershell.exe Get-Clipboard -Raw",
+    ["*"] = "wsl.exe powershell.exe Get-Clipboard -Raw",
+  },
+  cache_enabled = 0,
+}
