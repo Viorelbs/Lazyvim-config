5b1f71480f9ad7ad60dc66235ee67113f657ae28
Binciu Viorel <viorel.binciu@pdq.com>
2024-05-26 10:16:00 +0300

    feat: Added editor

---
+++ lua/plugins/editor.lua
@@ -0,0 +1,44 @@
+return {
+  { -- Configure flash to zip through results easier
+    "folke/flash.nvim",
+    opts = {
+      modes = {
+        search = {
+          enabled = false,
+        },
+        label = {
+          rainbow = true,
+        },
+        char = {
+          jump_labels = false,
+          autohide = true,
+        },
+      },
+    },
+  },
+  { -- Add indent indicators
+    "lukas-reineke/virt-column.nvim",
+    opts = {
+      char = "▎",
+      highlight = "CursorLineSign",
+      virtcolumn = "80,120",
+    },
+  },
+  { -- Auto save on focus lose
+    "tmillr/sos.nvim",
+    lazy = false,
+    config = function()
+      require("sos").setup()
+    end,
+  },
+  {
+    "folke/zen-mode.nvim",
+    opts = {
+      window = {
+        options = {
+          relativenumber = true,
+        },
+      },
+    },
+  },
+}
