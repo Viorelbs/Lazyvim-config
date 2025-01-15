c4952f9837f29a01550ba7040d200a1782e28d4d
Viorel Binciu <viorel.binciu@pdq.com>
2025-01-15 09:36:19 +0200

    task: Added snacks nvim

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -41,4 +41,37 @@ return {
       },
     },
   },
+  {
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    opts = {
+      dashboard = {
+        sections = {
+          { section = "header" },
+
+          { section = "keys", gap = 1, padding = 1 },
+          { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+          { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+          {
+            pane = 2,
+            icon = " ",
+            title = "Git Status",
+            section = "terminal",
+            enabled = vim.fn.isdirectory(".git") == 1,
+            cmd = "hub status --short --branch --renames",
+            height = 5,
+            padding = 1,
+            ttl = 5 * 60,
+            indent = 3,
+          },
+          { section = "startup" },
+        },
+      },
+    },
+  },
+  {
+    "folke/twilight.nvim",
+    opts = {},
+  },
 }
